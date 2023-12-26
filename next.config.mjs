8a67619288b0ca9f136c45e6de6e965ded66776c
Shaharyar <Shaharyar321321@gmail.com>
2023-12-26 10:59:25 +0500

    change to redeploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful for Docker builds.
  */
 await import("./src/env.mjs");
 import { withContentlayer } from "next-contentlayer";
