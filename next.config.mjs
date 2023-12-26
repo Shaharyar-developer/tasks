d2de6c2f440750a5514844a36adcd32055a2586a
Shaharyar <Shaharyar321321@gmail.com>
2023-12-26 11:17:44 +0500

    attempt fix dynamic import

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful for Docker builds.
  */
-import "./src/env.mjs";
-import { withContentlayer } from "next-contentlayer";
+await import("./src/env.mjs");
+const { withContentlayer } = await import("next-contentlayer");
 
 /** @type {import("next").NextConfig} */
 const config = {};
